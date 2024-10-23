0f5347f8f41c4ccd3e635f5cd9a4b67de00e630e
fst99 <96962890+fst99@users.noreply.github.com>
2024-10-23 17:16:07 +0200

    track ignored files

---
+++ Intermediate/Build/Win64/x64/BallanceUnrealEditor/Development/Engine/SharedPCH.Engine.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Engine/Public/EngineSharedPCH.h
+#include "E:/Ballance C++/BallanceUnreal/Intermediate/Build/Win64/x64/BallanceUnrealEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.h"
+#include "Runtime/Engine/Public/EngineSharedPCH.h"
---
+++ Intermediate/Build/Win64/x64/BallanceUnrealEditor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
+#include "E:/Ballance C++/BallanceUnreal/Intermediate/Build/Win64/x64/BallanceUnrealEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
+#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
